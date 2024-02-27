4564225e4b57162598483856b8d0ce1238623227
salvador <salvadoredgar99@gmail.com>
2024-02-27 10:58:36 -0600

    Add setup to tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,10 @@ dependencies {
     debugImplementation("com.google.dagger:hilt-compiler:2.50")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito.kotlin:mockito:5.3.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
