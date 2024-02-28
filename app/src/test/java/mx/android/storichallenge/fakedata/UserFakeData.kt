b2431601d2db6f8780e779419d4489cb961c3ba4
salvador <salvadoredgar99@gmail.com>
2024-02-27 19:10:14 -0600

    Add test to AuthExceptionHandler

--- app/src/test/java/mx/android/storichallenge/fakedata/UserFakeData.kt
+++ app/src/test/java/mx/android/storichallenge/fakedata/UserFakeData.kt
@@ -6,12 +6,13 @@ import mx.android.storichallenge.domain.model.FIRST_NAME
 import mx.android.storichallenge.domain.model.LAST_NAME
 import mx.android.storichallenge.domain.model.UserData
 import mx.android.storichallenge.domain.model.UserDataSubmit
+import mx.android.storichallenge.ui.singin.UserDataSubmitUi
 
 const val ANY_USER_ID = "MaKNkzR0ySOFAd9PS4IFxnW5rpX2"
 private const val ANY_FIRST_NAME = "Salvador"
 private const val ANY_INVALID_FIRST_NAME = "Sal"
-private const val ANY_LAST_LAST_NAME = "Maurilio"
-private const val ANY_INVALID_NAME = "Sal"
+private const val ANY_LAST_NAME = "Maurilio"
+private const val ANY_INVALID_LAST_NAME = "Mau"
 const val ANY_USER_EMAIL = "salvador@google.mx"
 const val ANY_INVALID_USER_EMAIL = "salvadorbuapap.mx"
 const val ANY_PASSWORD = "Admin1234_1"
@@ -20,69 +21,83 @@ private const val ANY_OTHER_PASSWORD = "Admin1234_12"
 
 fun givenUserDataMap() = mapOf(
     FIRST_NAME to ANY_FIRST_NAME,
-    LAST_NAME to ANY_LAST_LAST_NAME,
+    LAST_NAME to ANY_LAST_NAME,
     EMAIL_NAME to ANY_USER_EMAIL
 )
 
 fun givenUserDataSubmit() = UserDataSubmit(
     fistName = ANY_FIRST_NAME,
-    lastName = ANY_LAST_LAST_NAME,
+    lastName = ANY_LAST_NAME,
     email = ANY_USER_EMAIL,
     password = ANY_PASSWORD
 )
 
 fun givenUserDataResponse() = UserDataResponse(
     firstName = ANY_FIRST_NAME,
-    lastName = ANY_LAST_LAST_NAME,
+    lastName = ANY_LAST_NAME,
     email = ANY_USER_EMAIL,
     movements = givenMovementsResponseArray()
 )
 
 fun givenUserData() = UserData(
     firstName = ANY_FIRST_NAME,
-    lastName = ANY_LAST_LAST_NAME,
+    lastName = ANY_LAST_NAME,
     email = ANY_USER_EMAIL,
     movements = givenMovementList()
 )
 
-//fun givenUserCredentialsUi() = UserCredentialsUi(
-//    name = ANY_NAME,
-//    email = ANY_USER_EMAIL,
-//    password = ANY_PASSWORD,
-//    confirmPassword = ANY_PASSWORD
-//)
-//
-//fun givenUserCredentialsUiWithInvalidName() = UserCredentialsUi(
-//    name = ANY_INVALID_NAME,
-//    email = ANY_USER_EMAIL,
-//    password = ANY_PASSWORD,
-//    confirmPassword = ANY_PASSWORD
-//)
-//
-//fun givenUserCredentialsUiWithInvalidEmail() = UserCredentialsUi(
-//    name = ANY_NAME,
-//    email = ANY_INVALID_USER_EMAIL,
-//    password = ANY_PASSWORD,
-//    confirmPassword = ANY_PASSWORD
-//)
-//
-//fun givenUserCredentialsUiWithInvalidPassword() = UserCredentialsUi(
-//    name = ANY_NAME,
-//    email = ANY_USER_EMAIL,
-//    password = ANY_INVALID_PASSWORD,
-//    confirmPassword = ANY_PASSWORD
-//)
-//
-//fun givenUserCredentialsUiWithInvalidConfirmPassword() = UserCredentialsUi(
-//    name = ANY_NAME,
-//    email = ANY_USER_EMAIL,
-//    password = ANY_PASSWORD,
-//    confirmPassword = ANY_INVALID_PASSWORD
-//)
-//
-//fun givenUserCredentialsUiWithDifferentPasswords() = UserCredentialsUi(
-//    name = ANY_NAME,
-//    email = ANY_USER_EMAIL,
-//    password = ANY_PASSWORD,
-//    confirmPassword = ANY_OTHER_PASSWORD
-//)
+fun givenUserDataSubmitUi() = UserDataSubmitUi(
+    fistName = ANY_FIRST_NAME,
+    lastName = ANY_LAST_NAME,
+    email = ANY_USER_EMAIL,
+    password = ANY_PASSWORD,
+    confirmPassword = ANY_PASSWORD
+)
+
+fun givenUserDataSubmitUiWithInvalidFirstName() = UserDataSubmitUi(
+    fistName = ANY_INVALID_FIRST_NAME,
+    lastName = ANY_LAST_NAME,
+    email = ANY_USER_EMAIL,
+    password = ANY_PASSWORD,
+    confirmPassword = ANY_PASSWORD
+)
+
+fun givenUserDataSubmitUiWithInvalidLastName() = UserDataSubmitUi(
+    fistName = ANY_FIRST_NAME,
+    lastName = ANY_INVALID_LAST_NAME,
+    email = ANY_USER_EMAIL,
+    password = ANY_PASSWORD,
+    confirmPassword = ANY_PASSWORD
+)
+
+fun givenUserDataSubmitUiWithInvalidEmail() = UserDataSubmitUi(
+    fistName = ANY_FIRST_NAME,
+    lastName = ANY_LAST_NAME,
+    email = ANY_INVALID_USER_EMAIL,
+    password = ANY_PASSWORD,
+    confirmPassword = ANY_PASSWORD
+)
+
+fun givenUserDataSubmitUiWithInvalidPassword() = UserDataSubmitUi(
+    fistName = ANY_FIRST_NAME,
+    lastName = ANY_LAST_NAME,
+    email = ANY_USER_EMAIL,
+    password = ANY_INVALID_PASSWORD,
+    confirmPassword = ANY_PASSWORD
+)
+
+fun givenUserDataSubmitUiWithInvalidConfirmPassword() = UserDataSubmitUi(
+    fistName = ANY_FIRST_NAME,
+    lastName = ANY_LAST_NAME,
+    email = ANY_USER_EMAIL,
+    password = ANY_PASSWORD,
+    confirmPassword = ANY_INVALID_PASSWORD
+)
+
+fun givenUserDataSubmitUiWithDifferentPasswords() = UserDataSubmitUi(
+    fistName = ANY_FIRST_NAME,
+    lastName = ANY_LAST_NAME,
+    email = ANY_USER_EMAIL,
+    password = ANY_PASSWORD,
+    confirmPassword = ANY_OTHER_PASSWORD
+)
---
+++ app/src/test/java/mx/android/storichallenge/ui/exception/AuthExceptionHandler.kt
@@ -0,0 +1,120 @@
+package mx.android.storichallenge.ui.exception
+
+import mx.android.storichallenge.core.assertThatEquals
+import mx.android.storichallenge.fakedata.ANY_INVALID_PASSWORD
+import mx.android.storichallenge.fakedata.ANY_INVALID_USER_EMAIL
+import mx.android.storichallenge.fakedata.ANY_PASSWORD
+import mx.android.storichallenge.fakedata.ANY_USER_EMAIL
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUi
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUiWithDifferentPasswords
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUiWithInvalidConfirmPassword
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUiWithInvalidEmail
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUiWithInvalidFirstName
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUiWithInvalidLastName
+import mx.android.storichallenge.fakedata.givenUserDataSubmitUiWithInvalidPassword
+import org.junit.Before
+import org.junit.Test
+
+class AuthExceptionHandlerShould {
+
+    private lateinit var authExceptionHandler: AuthExceptionHandler
+
+    @Before
+    fun setup() {
+        authExceptionHandler = AuthExceptionHandler()
+    }
+
+    @Test
+    fun `return NoValidationNeededException exception when areInvalidSingUpCredentials is called and userDataSubmitUi are valid`() {
+        val userDataSubmitUi = givenUserDataSubmitUi()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.second, AuthUiException.NoValidationNeededException)
+        assertThatEquals(result.first, false)
+    }
+
+    @Test
+    fun `return FirstNameException when areInvalidSingUpCredentials is called and userDataSubmitUi has first name invalid`() {
+        val userDataSubmitUi = givenUserDataSubmitUiWithInvalidFirstName()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.first, true)
+        assertThatEquals(result.second, AuthUiException.FirstNameException)
+    }
+
+    @Test
+    fun `return LastNameException when areInvalidSingUpCredentials is called and userDataSubmitUi has last name invalid`() {
+        val userDataSubmitUi = givenUserDataSubmitUiWithInvalidLastName()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.first, true)
+        assertThatEquals(result.second, AuthUiException.LastNameException)
+    }
+
+    @Test
+    fun `return EmailException when areInvalidSingUpCredentials is called and userDataSubmitUi has email invalid`() {
+        val userDataSubmitUi = givenUserDataSubmitUiWithInvalidEmail()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.first, true)
+        assertThatEquals(result.second, AuthUiException.EmailException)
+    }
+
+    @Test
+    fun `return PasswordException when areInvalidSingUpCredentials is called and userDataSubmitUi has password invalid`() {
+        val userDataSubmitUi = givenUserDataSubmitUiWithInvalidPassword()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.first, true)
+        assertThatEquals(result.second, AuthUiException.PasswordException)
+    }
+
+    @Test
+    fun `return ConfirmPasswordException when areInvalidSingUpCredentials is called and userDataSubmitUi has confirm password invalid`() {
+        val userDataSubmitUi = givenUserDataSubmitUiWithInvalidConfirmPassword()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.first, true)
+        assertThatEquals(result.second, AuthUiException.ConfirmPasswordException)
+    }
+
+    @Test
+    fun `return DifferentPasswordException when areInvalidSingUpCredentials is called and userDataSubmitUi has different passwords invalid`() {
+        val userDataSubmitUi = givenUserDataSubmitUiWithDifferentPasswords()
+
+        val result = authExceptionHandler.areInvalidSingUpCredentials(userDataSubmitUi)
+
+        assertThatEquals(result.first, true)
+        assertThatEquals(result.second, AuthUiException.DifferentPasswordException)
+    }
+
+    @Test
+    fun `return NoValidationNeededException when areInvalidSingInCredentials is called and email and password are valid`() {
+        val result = authExceptionHandler.areInvalidSingInCredentials(ANY_USER_EMAIL, ANY_PASSWORD)
+
+        assertThatEquals(result.second, AuthUiException.NoValidationNeededException)
+        assertThatEquals(result.first, false)
+    }
+
+    @Test
+    fun `return EmailException when areInvalidSingInCredentials is called and email is valid`() {
+        val result = authExceptionHandler.areInvalidSingInCredentials(ANY_INVALID_USER_EMAIL, ANY_PASSWORD)
+
+        assertThatEquals(result.second, AuthUiException.EmailException)
+        assertThatEquals(result.first, true)
+    }
+
+    @Test
+    fun `return PasswordException when areInvalidSingInCredentials is called and password is valid`() {
+        val result = authExceptionHandler.areInvalidSingInCredentials(ANY_USER_EMAIL, ANY_INVALID_PASSWORD)
+
+        assertThatEquals(result.second, AuthUiException.PasswordException)
+        assertThatEquals(result.first, true)
+    }
+}
