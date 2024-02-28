073d458c112a1e0d3a97dd7e552fbcd7abb18336
salvador <salvadoredgar99@gmail.com>
2024-02-28 00:12:06 -0600

    Add theme to compose

--- app/src/main/java/mx/android/storichallenge/ui/singin/SingInActivity.kt
+++ app/src/main/java/mx/android/storichallenge/ui/singin/SingInActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -23,17 +20,15 @@ class SingInActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StoriChallengeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                LoginScreen("Android")
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun LoginScreen(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -42,8 +37,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginScreenPreview() {
     StoriChallengeTheme {
-        Greeting("Android")
+        LoginScreen("Android")
     }
 }
